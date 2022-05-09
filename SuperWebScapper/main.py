af65c144fe6ebb059025f72bee37c837e68b901c
hanbinChae <hanbin2043@gmail.com>
2022-05-09 13:03:49 +0900

    Using Fake Database

--- SuperWebScapper/main.py
+++ SuperWebScapper/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from scrapper import get_jobs
 
 app = Flask(__name__,template_folder='templates')
+db = {}
 
 @app.route("/")
 def home():
@@ -12,10 +13,16 @@ def report():
     word = request.args.get("word")
     if word:
         word = word.lower()
-        jobs = get_jobs(word)
-        print(jobs)
+        fromDb = db.get(word)
+        if fromDb:
+            jobs = fromDb
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
     else:
         return redirect('/')
-    return render_template("report.html",searchingBy=word)
+    return render_template("report.html"
+    ,searchingBy=word
+    ,resultsNumber=len(jobs))
 
 app.run(host="0.0.0.0",port =8080)
--- WebScapping func/main.py
+++ WebScapping func/main.py
@@ -1,6 +1,6 @@
 from indeed import get_jobs as get_indeed_jobs
 from StackOverflow import get_jobs as get_so_jobs
-from save import save_to_file
+from conversionExcel import save_to_file
 
 indeed_jobs = get_indeed_jobs()
 so_jobs = get_so_jobs()
