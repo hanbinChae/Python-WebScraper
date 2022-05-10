f7d201c2abf5bb9ca49b6d29441e8d00adf3646d
hanbinChae <hanbin2043@gmail.com>
2022-05-10 21:15:14 +0900

    Export Route

--- SuperWebScapper/main.py
+++ SuperWebScapper/main.py
@@ -28,4 +28,20 @@ def report():
         ,jobs = jobs
     )
 
-app.run(host="0.0.0.0",port =8080)
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get("word")
+        if not word:
+            raise Exception()
+        word = word.lower() 
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        return f"Generate CSV for {word}"
+    except:
+        return redirect("/");
+    
+        
+
+app.run(host="0.0.0.0",port =8081)
