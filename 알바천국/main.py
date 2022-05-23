eb81faeccf9031db8e73b8fdb59594e666b5c8b6
hanbinChae <hanbin2043@gmail.com>
2022-05-23 15:29:27 +0900

    location

--- 알바천국/main.py
+++ 알바천국/main.py
@@ -1,11 +1,9 @@
-from __future__ import print_function
-from multiprocessing import connection
 import requests
 from bs4 import BeautifulSoup
 
 alba_url = "http://www.alba.co.kr"
 
-def get_last_page(URL):
+def get_all_page(URL):
     result = requests.get(URL)
     soup = BeautifulSoup(result.text,"html.parser")
     pages = soup.find("div",id="MainSuperBrand").find_all("a")
@@ -14,16 +12,30 @@ def get_last_page(URL):
         RESULT.append(i['href'])
     return RESULT
 
-def convert_excel(links):
+def get_jobs(html):
+    if html.find("td",class_="local first") == None:
+        return 0;
+    else:
+        location = html.find("td",class_="local first").get_text()
+
+    return {"location":location}
+
+def extract_info(links):
+    jobs = []
     for link in set(links):
         try:
             r = requests.get(link)
         except:
             continue;
         s = BeautifulSoup(r.text,"html.parser")
-        p = s.find("div",id="NormalInfo").find("td",class_="local first ")
-        print(p)
+        p = s.find("div",id="NormalInfo").find_all("tr",class_="")
+        for each_p in p:
+            r = get_jobs(each_p)
+            jobs.append(r)
+    return jobs
 
-pages = get_last_page(alba_url) #링크별 url 리스트
-convert_excel(pages);
+testing = ['https://barogo.alba.co.kr/job/brand/main.asp']
+pages = get_all_page(alba_url) #링크별 url 리스트
+
+job = extract_info(testing);
 
